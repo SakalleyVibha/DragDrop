a648c3337d094913398752b61630d3ab4d8b7535
SakalleyVibha <vibhasakalley.covetus@gmail.com>
2024-03-15 19:49:49 +0530

    worked on setup

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { SettingPageComponent } from './setting-page/setting-page.component';
+
+export const routes: Routes = [
+    { path: '', component: SettingPageComponent }
+];
